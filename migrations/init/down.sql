1ec2c55e1913d4790e9c1fb82ca8f7947dfe2405
Nicholas Dolan <nick@nick-laptop>
2019-08-24 22:33:15 -0500

    temp(sql)

---
+++ migrations/init/down.sql
@@ -0,0 +1,7 @@
+DROP PROCEDURE "bb"."DrawNthCard";
+DROP PROCEDURE "bb"."GetCardRange";
+DROP TYPE "bb"."IdTable";
+DROP TABLE "bb"."DrawCard";
+DROP TABLE "bb"."Card";
+DROP TABLE "bb"."ParentSet";
+DROP SCHEMA "bb";
--- migrations/init/up.sql
+++ migrations/init/up.sql
@@ -1,25 +1,39 @@
 -- For database "biggest_blackest"
 CREATE SCHEMA "bb";
 
-CREATE TABLE "bb"."ParentSet" (
-  Id SERIAL NOT NULL CONSTRAINT PK_ParentSet PRIMARY KEY,
-  Name TEXT NOT NULL CONSTRAINT UX_ParentSet UNIQUE
+CREATE IF NOT EXISTS TABLE "bb"."User" (
+  Id SERIAL NOT NULL CONSTRAINT PK_User PRIMARY KEY,
+  Username TEXT NOT NULL CONSTRAINT UX_User_Name UNIQUE,
+  IsActive BOOLEAN NOT NULL DEFAULT TRUE,
+  LastModified TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE UTC)
 );
 
-CREATE TABLE "bb"."Card" (
+CREATE IF NOT EXISTS TABLE "bb"."ParentSet" (
+  Id SERIAL NOT NULL CONSTRAINT PK_ParentSet PRIMARY KEY,
+  Name TEXT NOT NULL CONSTRAINT UX_ParentSet_Name UNIQUE,
+  IsActive BOOLEAN NOT NULL DEFAULT TRUE,
+  LastModified TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE UTC)
+);
+
+CREATE IF NOT EXISTS TABLE "bb"."Card" (
   Id SERIAL NOT NULL CONSTRAINT PK_Card PRIMARY KEY,
   IsBlack BOOLEAN NOT NULL,
-  ParentSetId INT NOT NULL CONSTRAINT FK_Card_ParentSet REFERENCES "bb"."ParentSet",
-  FormatText TEXT NOT NULL
+  ParentSetId INT NOT NULL CONSTRAINT FK_Card_ParentSet REFERENCES "bb"."ParentSet"(Id),
+  FormatText TEXT NOT NULL,
+  IsActive BOOLEAN NOT NULL DEFAULT TRUE,
+  LastModified TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE UTC)
 );
 
-CREATE TABLE "bb"."DrawCard" (
+CREATE IF NOT EXISTS TABLE "bb"."DrawCard" (
   Id SERIAL NOT NULL CONSTRAINT PK_DrawCard PRIMARY KEY,
   CardId INT NOT NULL CONSTRAINT FK_DrawCard REFERENCES "bb"."Card"(Id),
-  DrawDate TIMESTAMP NOT NULL
+  DrawDate TIMESTAMP NOT NULL,
+  SessionKey TEXT,
+  IsActive BOOLEAN NOT NULL DEFAULT TRUE,
+  LastModified TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE UTC)
 );
 
-CREATE TYPE "bb"."IdTable" AS RANGE (
+CREATE IF NOT EXISTS TYPE "bb"."IdTable" AS RANGE (
   SUBTYPE = INT
 );
 
@@ -34,14 +48,49 @@ $$;
 
 CREATE OR REPLACE PROCEDURE "bb"."DrawNthCard"(n INT, sets "bb"."IdTable", is_black BOOLEAN)
 AS $$
-  SELECT
+  DECLARE r = SELECT
     c.Id,
     c.IsBlack,
     p.ParentSetId,
-    p.ParentSet,
+    p.Name AS "ParentSet",
     c.FormatText
   FROM "bb"."Card" AS c
     INNER JOIN @sets AS s ON s.Id = c.ParentSetId
     INNER JOIN "bb"."ParentSet" AS p ON p.Id = c.ParentSetId
-  WHERE IsBlack = @is_black AND ROW_NUMBER() = @n
+  WHERE IsBlack = @is_black AND ROW_NUMBER() OVER (ORDER BY Id) = @n
+$$;
+
+CREATE OR REPLACE PROCEDURE "bb"."GetCardById"(id INT)
+AS $$
+  SELECT
+    Id,
+    IsBlack,
+    p.ParentSetId,
+    p.Name AS "ParentSet",
+    FormatText
+  FROM "bb"."Card" AS c
+    INNER JOIN "bb"."ParentSet" AS p ON p.Id = c.ParentId
+  WHERE c.Id = id
+$$;
+
+CREATE OR REPLACE PROCEDURE "bb"."GetDecks"
+AS $$
+  SELECT
+    Id,
+    Name
+  FROM ParentSet
+$$;
+
+CREATE OR REPLACE PROCEDURE "bb"."GetCardsByDeck"(sets "bb"."IdTable", isBlack BOOLEAN)
+AS $$
+  SELECT
+    Id,
+    IsBlack,
+    p.ParentSetId,
+    p.Name AS "ParentSet",
+    FormatText
+  FROM "bb"."Card" AS c
+    INNER JOIN "bb"."ParentSet" AS p ON p.Id = c.ParentId
+    INNER JOIN sets AS s ON s.Id = p.Id
+  WHERE isBlack IS NULL OR c.IsBlack = isBlack
 $$;
