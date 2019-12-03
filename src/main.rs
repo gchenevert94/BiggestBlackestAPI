ff5af3f5c09d37adebdd9a8a815760c55455d3d3
Nick Dolan <nicholas.dolan1@gmail.com>
2019-12-02 19:35:25 -0600

    feat(juniper_from_schema):
    Implemented Juniper from schema and there should be no N+1 query bugs in the
    project. The schema definition is well defined now, and always terminates.
    
    TODO: Look back at the panics that are being unwinded in this project

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 #[macro_use]
 extern crate diesel;
+extern crate diesel_full_text_search;
+extern crate base64;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
@@ -10,57 +12,22 @@ extern crate juniper;
 extern crate juniper_from_schema;
 
 use std::io;
-use std::sync::Arc;
 
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use diesel::{
     r2d2::{Pool, ConnectionManager, PooledConnection},
     PgConnection
 };
-use futures::Future;
-use juniper::http::graphiql::graphiql_source;
-use juniper::http::GraphQLRequest;
 
 mod schema;
 mod models;
-mod gql_schema;
-
-use crate::gql_schema::{create_schema, Schema};
+mod gql;
 
 pub type DbPool = Pool<ConnectionManager<PgConnection>>;
 pub type DbCon = PooledConnection<ConnectionManager<PgConnection>>;
 
 pub struct Context {
     db_con: DbCon
-}
-
-fn graphiql() -> HttpResponse {
-    let html = graphiql_source("http://127.0.0.1:8080/graphql");
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(html)
-}
-
-fn graphql(
-    st: web::Data<Arc<Schema>>,
-    data: web::Json<GraphQLRequest>,
-    db_pool: web::Data<DbPool>
-) -> impl Future<Item = HttpResponse, Error = Error> {
-
-    let ctx = Context {
-        db_con: db_pool.get().unwrap()
-    };
-
-    web::block(move || {
-        let res = data.execute(&st, &ctx);
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-    .map_err(Error::from)
-    .and_then(|user| {
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(user))
-    })
 }
 
 fn main() -> io::Result<()> {
@@ -72,17 +39,13 @@ fn main() -> io::Result<()> {
 
     let db_pool = create_db_pool();
 
-    // Create Juniper schema
-    let schema = std::sync::Arc::new(create_schema());
-
     // Start http server
     HttpServer::new(move || {
         App::new()
-            .data(schema.clone())
             .data(db_pool.clone())
+            .configure(gql::register)
             .wrap(middleware::Logger::default())
-            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
-            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+            .default_service(web::to(|| "404"))
     })
     .bind("127.0.0.1:8080")?
     .run()
--- src/models.rs
+++ src/models.rs
@@ -1,61 +1,22 @@
 use chrono::{NaiveDateTime};
-use juniper_eager_loading::impl_load_from_for_diesel_pg;
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-
-use super::DbCon;
-use super::schema::bb::{card, parent_set, parent_set_card};
 
 #[derive(Clone, Queryable)]
 pub struct Card {
     pub id: i32,
     pub is_black: bool,
     pub format_text: String,
-    is_active: bool,
-    last_modified: NaiveDateTime
 }
 
 #[derive(Clone, Queryable)]
 pub struct ParentSet {
     pub id: i32,
     pub name: String,
-    is_active: bool,
-    last_modified: NaiveDateTime
 }
 
 #[derive(Queryable)]
 pub struct ParentSetCard {
-    parent_set_id: i32,
+    parentsetid: i32,
     card_id: i32,
     is_active: bool,
     last_modified: NaiveDateTime
-}
-
-impl juniper_eager_loading::LoadFrom<ParentSet> for Card {
-  type Error = diesel::result::Error;
-  type Connection = PgConnection;
-
-  fn load(
-    parent_sets: &[ParentSet],
-    _: &(),
-    ctx: &Self::Connection,
-  ) -> Result<Vec<Self>, Self::Error> {
-    use super::schema::bb::card::dsl::*;
-    use super::schema::bb::parent_set_card::dsl::*;
-
-    use diesel::pg::expression::dsl::any;
-
-    let parent_set_ids = parent_sets
-      .iter()
-      .map(|s| s.id)
-      .collect::<Vec<_>>();
-
-    let card_ids = parent_set_card
-      .filter(parentsetid.eq(any(parent_set_ids)))
-      .select(cardid);
-
-    card
-      .filter(id.eq(any(card_ids)))
-      .load::<Card>(ctx)
-  }
 }
